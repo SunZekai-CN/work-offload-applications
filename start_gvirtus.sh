15896740200ac754aac395be211ed73cc78bcf57
SunZekai-CN <sunzekai@mail.ustc.edu.cn>
2023-02-12 02:28:19 +0800

    update

--- start_gvirtus.sh
+++ start_gvirtus.sh
@@ -1,4 +1,3 @@
 export GVIRTUS_HOME=/opt/GVirtuS
 export GVIRTUS_LOGLEVEL=0
-vim $GVIRTUS_HOME/etc/properties.json
 LD_LIBRARY_PATH=$GVIRTUS_HOME/lib:$LD_LIBRARY_PATH $GVIRTUS_HOME/bin/gvirtus-backend $GVIRTUS_HOME/etc/properties.json
